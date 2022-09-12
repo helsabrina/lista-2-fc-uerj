65014fe3d2f07609f0137a72bfbf35b7f193a3d8
Sabrina <helsabrinacarvalho@hotmail.com>
2022-09-12 12:41:39 -0300

    finalizando exercicio

--- ex-6.py
+++ ex-6.py
@@ -1,22 +1,19 @@
-
-valor = list(input(str("Digite sua palavra/sequencia: ")))
-
-len_valor = len(valor)
-print(len_valor)
-
-if(len_valor < 2):
-    print(len_valor)
-    while(len_valor < 2):
-        print(len_valor)
-        valor = list(input(str("A palavra/sequencia contém apenas um dígito, por favor, digite algo válido: ")))
-
 def palindromo(palavra):
-    reverso = palavra[::-1]
+    nova_palavra = palavra.replace(" ", "")
+    lis_palavra = list(nova_palavra)
+    reverso = lis_palavra[::-1]
 
-    if(palavra == reverso):
+    if(lis_palavra == reverso):
         return True
     else:
         return False
+    
+valor = input(str("Digite sua palavra/sequencia: ")).lower()
+len_valor = len(valor)
+
+while(len_valor < 2):
+    valor = input(str("A palavra/sequencia contém apenas um dígito, por favor, digite uma frase/palavra válida: ")).lower()
+    len_valor = len(valor)
 
 resultado = palindromo(valor)
 print(resultado)
